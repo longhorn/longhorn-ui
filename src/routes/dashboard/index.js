05ebe3e56b0cc66a5d460692c2bf277eb1a7a6e9
loganhz <logan@rancher.com>
2017-04-11 17:52:54 +0800

    bug fix

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'dva'
-import { Row, Col, Card } from 'antd'
-import { NumberCard, EventLogs } from './components'
+import { Row, Col } from 'antd'
+import { NumberCard } from './components'
 
 function Dashboard({ host, volume }) {
   const hostNum = host.data.length
@@ -25,16 +25,6 @@ function Dashboard({ host, volume }) {
   const numberCards = numbers.map((item, key) => <Col key={key} lg={12} md={12}>
     <NumberCard {...item} />
   </Col>)
-
-  const eventLogs = {
-    data: [
-      { date: '3/14/2017 5:24:44pm', type: 'info', text: 'Info replica replia-0scb43-1 rebuild complete' },
-      { date: '3/14/2017 5:24:44pm', type: 'error', text: 'Error replica replica-0scb43-1 rebuild faliure' },
-      { date: '3/14/2017 5:24:44pm', type: 'info', text: 'Info replica replia-0scb43-1 rebuild complete' },
-      { date: '3/14/2017 5:24:44pm', type: 'info', text: 'Info replica replia-0scb43-1 rebuild complete' },
-      { date: '3/14/2017 5:24:44pm', type: 'info', text: 'Info replica replia-0scb43-1 rebuild complete' },
-    ],
-  }
 
   return (
     <div>
