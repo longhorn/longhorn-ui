426f13fbf0090c06e4c8aeaefabef972c3f5d337
LiuYan <361112237@qq.com>
2018-07-26 08:59:04 +0800

    remove volume duplicate code

--- src/routes/volume/helper/index.js
+++ src/routes/volume/helper/index.js
@@ -3,3 +3,47 @@ export function isSchedulingFailure(volume) {
   // volume.conditions may be empty object({})
   return volume.conditions && volume.conditions.scheduled && volume.conditions.scheduled.status.toLowerCase() === 'false'
 }
+
+export function genAttachHostModalProps(volumes, hosts, visible, dispatch) {
+  return {
+    items: volumes,
+    visible,
+    hosts,
+    onOk(selectedHost, urls) {
+      dispatch({
+        type: 'volume/attach',
+        payload: {
+          host: selectedHost,
+          url: urls[0],
+        },
+      })
+    },
+    onCancel() {
+      dispatch({
+        type: 'volume/hideAttachHostModal',
+      })
+    },
+  }
+}
+
+export function getEngineUpgradeModalProps(volumes, engineImages, visible, dispatch) {
+  return {
+    items: volumes,
+    visible,
+    engineImages,
+    onOk(image, urls) {
+      dispatch({
+        type: 'volume/engineUpgrade',
+        payload: {
+          image,
+          url: urls[0],
+        },
+      })
+    },
+    onCancel() {
+      dispatch({
+        type: 'volume/hideEngineUpgradeModal',
+      })
+    },
+  }
+}
