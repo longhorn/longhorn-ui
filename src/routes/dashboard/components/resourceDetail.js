8d3d35fd6f9d1a16d620a9e58a472e35c4cc49ee
LiuYan <361112237@qq.com>
2018-08-10 17:21:03 +0800

    Hover the item in the detail, highlight the item in the dial

--- src/routes/dashboard/components/resourceDetail.js
+++ src/routes/dashboard/components/resourceDetail.js
@@ -2,16 +2,16 @@ import React, { PropTypes } from 'react'
 import classnames from 'classnames'
 import styles from './resourceDetail.less'
 
-function ResourceDetail({ data, total, width = 300, onClick = f => f, clickable }) {
+function ResourceDetail({ data, total, width = 300, onClick = f => f, onMouseEnter = f => f, onMouseLeave = f => f, clickable }) {
   return (
     <div className={styles.resourceDetailContainer}>
       <div className={styles.resourceDetail} style={{ width: `${width}px` }}>
         <div className={styles.resourceDetailBody}>
-        {data.map(d => (
-          <div key={d.name} className={classnames(styles.detailItem, { [styles.clickable]: clickable })}>
+        {data.map((d, index) => (
+          <div key={d.name} className={classnames(styles.detailItem, { [styles.clickable]: clickable })} onMouseEnter={e => onMouseEnter(d, index, e)} onMouseLeave={e => onMouseLeave(d, index, e)} >
             <div className={styles.detailContent} onClick={() => onClick(d)}>
               <div className={styles.label}><div className={styles.badge} style={{ backgroundColor: d.color }}></div>{d.name}</div>
-              <div className={styles.value}>{d.value}</div>
+              <div>{d.value}</div>
             </div>
           </div>
         ))}
@@ -32,6 +32,8 @@ ResourceDetail.propTypes = {
   width: PropTypes.number,
   onClick: PropTypes.func,
   clickable: PropTypes.bool,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
 }
 
 export default ResourceDetail
