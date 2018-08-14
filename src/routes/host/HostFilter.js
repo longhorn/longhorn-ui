9fe86ea1e032c9467638bcde98d8399d299bc91e
LiuYan <361112237@qq.com>
2018-08-14 15:44:43 +0800

    add expand all feature

--- src/routes/host/HostFilter.js
+++ src/routes/host/HostFilter.js
@@ -1,28 +1,49 @@
 import React, { PropTypes } from 'react'
-import { Form, Row, Col } from 'antd'
+import { Row, Col, Button } from 'antd'
 import { Filter } from '../../components/index'
+import styles from './HostFilter.less'
 
-const HostFilter = ({
-  location,
-  onSearch,
-  stateOption,
-  fieldOption,
-}) => {
-  const searchGroupProps = {
-    location,
-    stateOption,
-    fieldOption,
-    onSearch: (value) => {
-      onSearch(value)
-    },
+class HostFilter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isAllExpanded: false,
+    }
   }
-  return (
-    <Row gutter={24}>
-      <Col lg={{ offset: 18, span: 6 }} md={{ offset: 16, span: 8 }} sm={{ offset: 8, span: 16 }} xs={24} style={{ marginBottom: 16 }}>
-        <Filter {...searchGroupProps} />
-      </Col>
-    </Row>
-  )
+  toggleExpand = (isAllExpanded) => {
+    if (isAllExpanded !== undefined) {
+      this.setState({ isAllExpanded })
+      return
+    }
+    const { expandAll = f => f, collapseAll = f => f } = this.props
+    if (this.state.isAllExpanded) {
+      collapseAll()
+    } else {
+      expandAll()
+    }
+    this.setState({ isAllExpanded: !this.state.isAllExpanded })
+  }
+  render() {
+    const { location, onSearch, stateOption, fieldOption } = this.props
+    const searchGroupProps = {
+      location,
+      stateOption,
+      fieldOption,
+      onSearch: (value) => {
+        onSearch(value)
+      },
+    }
+    return (
+      <Row gutter={24}>
+        <Col lg={18} md={16} sm={24} xs={24}>
+          <Button size="large" className={styles.expandOrCollapseButton} onClick={() => this.toggleExpand()}>{this.state.isAllExpanded ? 'Collapse' : 'Expand'} All</Button>
+        </Col>
+        <Col lg={6} md={8} sm={24} xs={24} style={{ marginBottom: 16 }}>
+          <Filter {...searchGroupProps} />
+        </Col>
+      </Row>
+    )
+  }
 }
 
 HostFilter.propTypes = {
@@ -30,6 +51,8 @@ HostFilter.propTypes = {
   location: PropTypes.object,
   stateOption: PropTypes.array,
   fieldOption: PropTypes.array,
+  expandAll: PropTypes.func,
+  collapseAll: PropTypes.func,
 }
 
-export default Form.create()(HostFilter)
+export default HostFilter
