f377a15adfc3b297e38eda6599104f237477f6d2
James Oliver <joliver@rancher.com>
2018-11-07 20:51:04 -0700

    Upgrade checker support
    https://github.com/rancher/longhorn/issues/312
    
    - Use checkbox for setting values of bool type

--- src/routes/setting/setting.js
+++ src/routes/setting/setting.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import { Form, Input, Button, Spin, Icon } from 'antd'
+import { Form, Input, Button, Spin, Icon, Checkbox } from 'antd'
 import styles from './setting.less'
 const FormItem = Form.Item
 
@@ -15,6 +15,7 @@ const form = ({
 }) => {
   const handleOnSubmit = () => {
     const fields = getFieldsValue()
+    Object.keys(fields).forEach(key => { fields[key] = fields[key].toString() })
     onSubmit(fields)
   }
   const parseSettingRules = (setting) => {
@@ -26,16 +27,42 @@ const form = ({
     return rules
   }
   const genInputItem = (setting) => {
-    return (<Input readOnly={setting.definition.readOnly} />)
-  }
-  const genFormItem = (setting) => <FormItem key={setting.id} label={<span style={{ fontSize: '14px' }}>{setting.definition.displayName}</span>} >
-    {getFieldDecorator(setting.name, {
-      rules: parseSettingRules(setting),
-      initialValue: setting.value || setting.definition.default,
-    })(genInputItem(setting))
+    switch (setting.definition.type) {
+      case 'bool':
+        return (<Checkbox disabled={setting.definition.readOnly} />)
+      default:
+        return (<Input readOnly={setting.definition.readOnly} checked={setting.value} />)
     }
-    <Icon type="question-circle-o" /> &nbsp;<small style={{ color: '#6c757d', fontSize: '13px', fontWeight: 400 }}>{setting.definition.required && !setting.definition.readOnly ? 'Required. ' : ''}{setting.definition.description}</small>
-  </FormItem>
+  }
+  const genFormItem = (setting) => {
+    let initialValue
+    let valuePropName
+    switch (setting.definition.type) {
+      case 'bool':
+        if (setting.value !== '') {
+          initialValue = setting.value === 'true'
+        } else {
+          initialValue = setting.definition.default === 'true'
+        }
+        valuePropName = 'checked'
+        break
+      default:
+        initialValue = setting.value || setting.definition.default
+        valuePropName = 'value'
+        break
+    }
+    return (
+      <FormItem key={setting.id} label={<span style={{ fontSize: '14px' }}>{setting.definition.displayName}</span>} >
+        {getFieldDecorator(setting.name, {
+          rules: parseSettingRules(setting),
+          initialValue,
+          valuePropName,
+        })(genInputItem(setting))
+        }
+        <Icon type="question-circle-o" /> &nbsp;<small style={{ color: '#6c757d', fontSize: '13px', fontWeight: 400 }}>{setting.definition.required && !setting.definition.readOnly ? 'Required. ' : ''}{setting.definition.description}</small>
+      </FormItem>
+    )
+  }
   const getCategoryWeight = (category) => {
     switch (category) {
       case 'general':
