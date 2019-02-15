8cabd5809ca4f968307bd1b53f1528ec8b811988
LiuYan <361112237@qq.com>
2019-02-15 11:15:51 +0800

    update favicon.ico

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Longhorn UI',
   prefix: 'antdAdmin',
-  logoSrc: 'http://releases.cnrancher.com:9090/latest/assets/images/logos/favicon.ico',
+  logoSrc: 'favicon.ico',
   version: '${VERSION}', // eslint-disable-line no-template-curly-in-string
 }
