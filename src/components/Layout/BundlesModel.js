7954aa55d24ace6c87b54a880020be9d169c650f
siye <503630985@qq.com>
2019-08-02 09:45:14 +0800

    Add validation of space

--- src/components/Layout/BundlesModel.js
+++ src/components/Layout/BundlesModel.js
@@ -63,6 +63,7 @@ const modal = ({
               rules: [
                 {
                   required: true,
+                  whitespace: true,
                   message: "Please fill in support bundle's issue description",
                 },
               ],
