ac32e6b850304eee8214f2b8242bccfd320fa44c
Siye Wang <siye@rancher.com>
2019-09-14 09:53:38 +0800

    Fix the bug node tags will mess up Longhorn UI
    Signed-off-by: Siye Wang <siye@rancher.com>

--- src/routes/host/components/EditableDiskItem.js
+++ src/routes/host/components/EditableDiskItem.js
@@ -29,12 +29,12 @@ function EditableDiskItem({ isNew, disk, form, onRestore, onRemove, validatePath
 
   return (
     <div style={{ position: 'relative' }} className={classnames(styles.ediableDisk, { [styles.rowDeleted]: disk.deleted })}>
-      <div style={{ position: 'absolute', left: 30, top: 128 }}>
+      <div style={{ width: '83%', padding: '0px 15px', lineHeight: '40px' }}>
         {getFieldDecorator(`disks['${disk.id}']['tags']`, {
           initialValue: disk.tags,
         })(<DistTag tags={disk.tags} changeTags={(tags) => { form.setFieldsValue({ [`disks['${disk.id}']['tags']`]: tags }) }} />)}
       </div>
-      <div className={styles.formItem} style={{ width: '450px' }}>
+      <div className={styles.formItem} style={{ width: '83%' }}>
         <div className={styles.label}>
           Path
         </div>
