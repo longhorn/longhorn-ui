d48e2a555441ccf140e49c7c515ea2584359a715
Siye Wang <siye@rancher.com>
2019-11-15 10:16:00 +0800

    Fix(actions) Disabled upgrade button when the volume is not detached
    Signed-off-by: Siye Wang <siye@rancher.com>

--- src/routes/volume/VolumeBulkActions.js
+++ src/routes/volume/VolumeBulkActions.js
@@ -43,6 +43,7 @@ function bulkActions({ selectedRows, engineImages, bulkDeleteVolume, showBulkEng
   const hasDoingState = (exclusions = []) => selectedRows.some(item => (item.state.endsWith('ing') && !exclusions.includes(item.state)) || item.currentImage !== item.engineImage)
   const isSnapshotDisabled = () => selectedRows.every(item => !item.actions || !item.actions.snapshotCreate)
   const isHasStandy = () => selectedRows.some(item => item.standby)
+  const isAttached = () => selectedRows.some(item => item.state === 'attached')
   const isRestoring = () => selectedRows.some((selected) => {
     if (selected.restoreStatus && selected.restoreStatus.length > 0) {
       let flag = selected.restoreStatus.every((item) => {
@@ -62,7 +63,7 @@ function bulkActions({ selectedRows, engineImages, bulkDeleteVolume, showBulkEng
     { key: 'attach', name: 'Attach', disabled() { return selectedRows.length === 0 || !hasAction('attach') || hasDoingState() || isRestoring() } },
     { key: 'detach', name: 'Detach', disabled() { return selectedRows.length === 0 || !hasAction('detach') || hasDoingState(['attaching']) || isHasStandy() || isRestoring() } },
     { key: 'backup', name: 'Create Backup', disabled() { return selectedRows.length === 0 || isSnapshotDisabled() || hasDoingState() || isHasStandy() || isRestoring() } },
-    { key: 'upgrade', name: 'Upgrade Engine', disabled() { return selectedRows.length === 0 || !hasAction('engineUpgrade') || hasDoingState() || hasMoreOptions() || isRestoring() } },
+    { key: 'upgrade', name: 'Upgrade Engine', disabled() { return selectedRows.length === 0 || !hasAction('engineUpgrade') || hasDoingState() || hasMoreOptions() || isRestoring() || isAttached() } },
     { key: 'createPVAndPVC', name: 'Create PV/PVC', disabled() { return selectedRows.length === 0 || isHasStandy() || isRestoring() } },
   ]
 
