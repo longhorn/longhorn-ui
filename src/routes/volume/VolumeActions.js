95bb9a1eee691daf1c5aeb28e66e8fd09286b989
Siye Wang <siye@rancher.com>
2021-01-08 11:18:16 +0800

    fix(volume) Add detach tooltip message for RWX volume
    Signed-off-by: Siye Wang <siye@rancher.com>

--- src/routes/volume/VolumeActions.js
+++ src/routes/volume/VolumeActions.js
@@ -133,6 +133,10 @@ function actions({ selected, engineImages, showAttachHost, detach, showEngineUpg
     }
   }
 
+  const isRwxVolumeWithWorkload = () => {
+    return selected.accessMode === 'rwx' && selected.kubernetesStatus.workloadsStatus && selected.kubernetesStatus.workloadsStatus.length > 0
+  }
+
   const canUpdateDataLocality = () => {
     return selected.actions && selected.actions.updateDataLocality
   }
@@ -143,7 +147,7 @@ function actions({ selected, engineImages, showAttachHost, detach, showEngineUpg
 
   const allActions = [
     { key: 'attach', name: 'Attach', disabled: isRestoring() },
-    { key: 'detach', name: 'Detach', disabled: selected.standby || isRestoring() },
+    { key: 'detach', name: 'Detach', disabled: selected.standby || isRestoring() || isRwxVolumeWithWorkload(), tooltip: isRwxVolumeWithWorkload() ? 'The volume access mode is `ReadWriteMany`, Please ensure that the workloads are scaled down before trying to detach the volume' : '' },
     { key: 'salvage', name: 'Salvage', disabled: isRestoring() },
     { key: 'engineUpgrade', name: 'Upgrade Engine', disabled: (engineImages.findIndex(engineImage => selected.engineImage !== engineImage.image && engineImage.state === 'ready') === -1) || isRestoring() || (selected.state !== 'detached' && selected.state !== 'attached') },
     { key: 'updateReplicaCount', name: 'Update Replicas Count', disabled: selected.state !== 'attached' || isRestoring() || selected.standby },
