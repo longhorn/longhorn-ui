3a81cb5160325d029fab6c421a511da7f72c97d6
Siye Wang <siye@rancher.com>
2021-11-10 14:14:36 +0800

    fix(date) Eliminate browser's console warning message
    Signed-off-by: Siye Wang <siye@rancher.com>

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -2,11 +2,23 @@ import React from 'react'
 import moment from 'moment'
 import { Tooltip } from 'antd'
 
+function utcStrToDate(utcStr) {
+  const reg = /^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}) \+\d{4} UTC$/
+  const results = utcStr.match(reg)
+  if (results && results.length === 3) {
+    const d = results[1].split('-').map(item => parseInt(item, 10))
+    const t = results[2].split(':').map(item => parseInt(item, 10))
+    return new Date(Date.UTC(d[0], d[1] - 1, d[2], t[0], t[1], t[2]))
+  }
+  return utcStr
+}
+
 export function formatDate(date, hasTooltip = true) {
+  let gmt = utcStrToDate(date)
   if (hasTooltip) {
-    return <Tooltip title={`${moment(`${date}`).utc().format()}`}>
-      {moment(new Date(date)).fromNow()}
+    return <Tooltip title={`${moment(gmt).utc().format()}`}>
+      {moment(gmt).fromNow()}
     </Tooltip>
   }
-  return moment(date).fromNow()
+  return moment(gmt).fromNow()
 }
