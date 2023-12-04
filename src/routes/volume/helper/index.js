7fa0321f2c4ad142a3b9caacdbfe7ea6359f6ca1
Moritz Röhrich <moritz.rohrich@suse.com>
2023-12-04 16:50:30 +0100

    fix: display volume conditions
    Fix displaying volume scheduling conditions in volume list and volume
    details page.
    
    Fixes: #7241
    
    Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>
    (cherry picked from commit 6572cfd6d3ef89b5abd487c371ca8210f304a412)

--- src/routes/volume/helper/index.js
+++ src/routes/volume/helper/index.js
@@ -1,7 +1,7 @@
 export function isSchedulingFailure(volume) {
   // volume.conditions.scheduled.status may be equal to 'Unknown' or 'True' or 'False', only 'False' value is scheduling failure
   // volume.conditions may be empty object({})
-  return volume.conditions && volume.conditions.scheduled && volume.conditions.scheduled.status.toLowerCase() === 'false'
+  return volume.conditions && volume.conditions.Scheduled && volume.conditions.Scheduled.status.toLowerCase() === 'false'
 }
 
 export function genAttachHostModalProps(volumes, hosts, visible, dispatch) {
