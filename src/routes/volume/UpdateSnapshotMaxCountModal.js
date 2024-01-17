e800b8e20568b60b4a981f5e9154438e0b580c75
scures <scurescu@suse.com>
2024-01-17 11:13:32 +0100

    Enables global setting, volume creation and volume update fields for maxCount and maxSize
    Signed-off-by: scures <scurescu@suse.com>
    (cherry picked from commit d2e6e3129cb52187b49a9e2c5d1d3a33341f77f6)

---
+++ src/routes/volume/UpdateSnapshotMaxCountModal.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Form, InputNumber } from 'antd'
+import { ModalBlur } from '../../components'
+const FormItem = Form.Item
+
+const formItemLayout = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 12,
+  },
+}
+
+const modal = ({
+  item,
+  visible,
+  onCancel,
+  onOk,
+  form: { getFieldDecorator, getFieldsValue, validateFields },
+}) => {
+  function handleOk() {
+    validateFields((errors) => {
+      if (errors) {
+        return
+      }
+      const url = item && item.actions && item.actions.updateSnapshotMaxCount
+      const data = {
+        snapshotMaxCount: getFieldsValue().snapshotMaxCount,
+      }
+
+      onOk(data, url)
+    })
+  }
+  const modalOpts = {
+    title: 'Update Snapshot Max Count',
+    visible,
+    onCancel,
+    onOk: handleOk,
+  }
+
+  return (
+    <ModalBlur {...modalOpts}>
+      <Form
+        layout="horizontal"
+        {...formItemLayout}
+        style={{ display: 'flex', gap: 10 }}
+      >
+        <FormItem label="Max Count">
+          {getFieldDecorator('snapshotMaxCount', {
+            initialValue: item.snapshotMaxCount,
+            rules: [
+              {
+                required: true,
+                message: 'Please input volume snapshotMaxCount',
+              },
+              {
+                validator: (rule, value, callback) => {
+                  if (value < 2 || value > 250 || !/^\d+$/.test(value)) {
+                    callback(
+                      'The value should be an integer between 2 and 250'
+                    )
+                  } else {
+                    callback()
+                  }
+                },
+              },
+            ],
+          })(<InputNumber style={{ width: '270px' }} />)}
+        </FormItem>
+      </Form>
+    </ModalBlur>
+  )
+}
+
+modal.propTypes = {
+  form: PropTypes.object.isRequired,
+  visible: PropTypes.bool,
+  onCancel: PropTypes.func,
+  item: PropTypes.object,
+  selected: PropTypes.object,
+  onOk: PropTypes.func,
+  hosts: PropTypes.array,
+}
+
+export default Form.create()(modal)
