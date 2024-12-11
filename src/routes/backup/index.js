c324c484e74deaf180f40f255add42ddd6a8180d
Yi-Ya Chen <yiya.chen@suse.com>
2024-12-11 15:47:00 +0800

    feat: add Tabs to page
    Signed-off-by: Yi-Ya Chen <yiya.chen@suse.com>

--- src/routes/backup/index.js
+++ src/routes/backup/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Tabs } from 'antd'
 import VolumeBackup from './VolumeBackup'
+import styles from './index.less'
 
 function Backup({ dispatch, location }) {
+  const tabs = [
+    {
+      key: 'volume',
+      label: 'Volume',
+      content: <VolumeBackup dispatch={dispatch} location={location} />,
+    },
+    {
+      key: 'backing-image',
+      label: 'Backing Image',
+      content: 'Content of Tab Pane 2',
+    }
+  ]
+
   return (
-    <VolumeBackup dispatch={dispatch} location={location} />
+    <Tabs className={styles.container} defaultActiveKey={tabs[0].key} type="card">
+      {tabs.map((tab) => (
+        <Tabs.TabPane tab={tab.label} key={tab.key}>
+          {tab.content}
+        </Tabs.TabPane>
+      ))}
+    </Tabs>
   )
 }
 
