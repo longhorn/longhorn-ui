c0a52d930b7f25835a8e90ed8a4bd1fff3f40e1b
Yi-Ya Chen <yiya.chen@suse.com>
2024-12-24 14:20:55 +0800

    fix: update matches regex
    Signed-off-by: Yi-Ya Chen <yiya.chen@suse.com>
    (cherry picked from commit 8a4f4e7e156cffd6766ea38828f43ef1ae015a3e)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,12 @@
 const C = {
   RegExp: {
-    // Regular expressions ensure that only the correct request path is included
-    REQUEST: /^(https|wss)?.+?(:\d{2,6})?(?=\/v1)/,
+    // Regular expressions ensure that only the correct request path is included, scenarios:
+    // https://example.com/v1/backingimages -> /v1/backingimages
+    // http://localhost/v1/backingimages -> /v1/backingimages
+    // wss://example.com/v1/backingimages -> /v1/backingimages
+    // /v1/backingimages/v1?action=backupBackingImageCreate (no change)
+    // /v1/backupbackingimages/v1-xxx (no change)
+    REQUEST: /^(https?|wss?):\/\/[^/]+(:\d{2,6})?(?=\/v1)/,
   },
   ContainerMarginHeight: 101,
 }
